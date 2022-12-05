946d1ebde8e8b3d5836df506cd53804eb4475e36
S010MON <leon_debnath@hotmail.co.uk>
2022-12-05 19:43:42 +0100

    First attempt at simulation build

--- src/main/java/org/ssa/Simulation.java
+++ src/main/java/org/ssa/Simulation.java
@@ -15,22 +15,42 @@ public class Simulation {
     public Machine mach;
 	
 
-        /**
+    /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
     	// Create an eventlist
-	CEventList l = new CEventList();
-	// A queue for the machine
-	Queue q = new Queue();
-	// A source
-	Source s = new Source(q,l,"Source 1");
-	// A sink
-	Sink si = new Sink("Sink 1");
-	// A machine
-	Machine m = new Machine(q,si,l,"Machine 1");
-	// start the eventlist
-	l.start(2000); // 2000 is maximum time
+        CEventList l = new CEventList();
+        // A queue for the machine
+        Queue q1 = new Queue();
+        Queue q2 = new Queue();
+        Queue q3 = new Queue();
+        Queue q4 = new Queue();
+        Queue q5 = new Queue();
+        Queue q6 = new Queue();
+        Queue q7 = new Queue();
+
+        // A source
+        Source s1 = new Source(q1,l,"Source 1");
+        Source s2 = new Source(q2,l,"Source 2");
+        Source s3 = new Source(q3,l,"Source 3");
+        Source s4 = new Source(q4,l,"Source 4");
+        Source s5 = new Source(q5,l,"Source 5");
+        Source s6 = new Source(q6,l,"Source 6");
+        Source s7 = new Source(q7,l,"Source 7");
+
+        // A sink
+        Sink si = new Sink("Sink 1");
+        // A machine
+        Machine ambulance1 = new Machine(q1,si,l,"Ambulance 1");
+        Machine ambulance2 = new Machine(q1,si,l,"Ambulance 2");
+        Machine ambulance3 = new Machine(q1,si,l,"Ambulance 3");
+        Machine ambulance4 = new Machine(q1,si,l,"Ambulance 4");
+        Machine ambulance5 = new Machine(q1,si,l,"Ambulance 5");
+
+        // start the eventlist
+        l.start(2000); // 2000 is maximum time
     }
     
 }
